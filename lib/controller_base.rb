13ae16f1db8c6569a1fc77934d3c3af29ae9ecd9
paulmoliva <paulmoliva@gmail.com>
2016-08-09 14:07:41 -0700

    template render

---
+++ lib/controller_base.rb
@@ -0,0 +1,59 @@
+require 'active_support'
+require 'active_support/core_ext'
+require 'erb'
+require_relative './session'
+require 'byebug'
+class ControllerBase
+  attr_reader :req, :res, :params
+
+  # Setup the controller
+  def initialize(req, res)
+    @req = req
+    @res = res
+  end
+
+  # Helper method to alias @already_built_response
+  def already_built_response?
+    @already_built_response
+  end
+
+  # Set the response status code and header
+  def redirect_to(url)
+    raise 'double render' if already_built_response?
+    @res['location']=(url)
+    @res.status = 302
+    @already_built_response = true
+  end
+
+  # Populate the response with content.
+  # Set the response's content type to the given type.
+  # Raise an error if the developer tries to double render.
+  def render_content(content, content_type)
+    raise 'double render' if already_built_response?
+    @res['Content-Type'] = content_type
+    @res.write(content)
+    @already_built_response = true
+  end
+
+  # use ERB and binding to evaluate templates
+  # pass the rendered html to render_content
+  def render(template_name)
+    loc = File.dirname(__FILE__)
+    loc = loc.split('/')[0..-2].join('/')
+    loc += "/views/"
+    loc += self.class.name.match(/^[A-Z][a-z]+/).to_s.downcase
+    loc += '_controller/'
+    template_content = File.read(loc + template_name.to_s + '.html.erb')
+    template_content = ERB.new(template_content)
+    render_content(template_content.result(binding), 'text/html')
+
+  end
+
+  # method exposing a `Session` object
+  def session
+  end
+
+  # use this with the router to call action_name (:index, :show, :create...)
+  def invoke_action(name)
+  end
+end
