7bf11854791bff6962050ba631854257df071e04
paulmoliva <paulmoliva@gmail.com>
2016-08-10 12:11:01 -0700

    made exceptions page

--- lib/flash.rb
+++ lib/flash.rb
@@ -1,4 +1,34 @@
 require 'json'
 
 class Flash
+  def initialize(req)
+    @req = req
+    cookie = req.cookies["_rails_lite_app_flash"]
+    if cookie
+      @now = JSON.parse(cookie)
+      @later = {}
+    else
+      @now = {}
+      @later = {}
+    end
+  end
+
+  def [](key)
+    @now[key] || @later[key]
+  end
+
+  def []=(key, val)
+    @later[key] = val
+  end
+
+  def now
+    @now
+  end
+
+
+  # serialize the hash into json and save in a cookie
+  # add to the responses cookies
+  def store_flash(res)
+    res.set_cookie("_rails_lite_app_flash", JSON.generate(@later))
+  end
 end
--- lib/show_exceptions.rb
+++ lib/show_exceptions.rb
@@ -1,15 +1,34 @@
 require 'erb'
-
+require 'byebug'
+require 'rack'
 class ShowExceptions
+  attr_reader :app
   def initialize(app)
+    @app = app
   end
 
   def call(env)
+    begin
+      app.call(env)
+    rescue => e
+      render_exception(e)
+      # env.status = 500
+      # env['Content-Type'] = 'text'
+    end
   end
 
   private
 
   def render_exception(e)
+    res = Rack::Response.new
+    res.status = 500
+    res['Content-type'] = "text/html"
+    line_number = /\:\d+/.match(e.backtrace.to_s)
+    file_name = /[\w|\/|\-|\_]+\/\w+\.rb/.match(e.backtrace.to_s)
+    name = File.expand_path("views/exception_helper.html.erb")
+    output = ERB.new(File.read(name)).result(binding)
+    res.write(output)
+    res.finish
   end
 
 end
